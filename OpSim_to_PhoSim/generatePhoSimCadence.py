ccbf758076105dae1bfda36847bc1be65aa4d290
Nan Li <linan7788626@gmail.com>
2016-01-22 19:42:03 -0600

    new for OpSim and PhoSim

--- OpSim_to_PhoSim/generatePhoSimCadence.py
+++ OpSim_to_PhoSim/generatePhoSimCadence.py
@@ -5,6 +5,13 @@ from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
 from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogPoint
 from lsst.sims.catUtils.baseCatalogModels import StarObj
 
+#-----------------------------------------------------
+#from astropy.time import Time
+#times= ['1999-01-01T00:00:00.123456789','2010-01-01T00:00:00']
+#t = Time(times,format='isot',scale='utc')
+#t.mjd
+#-----------------------------------------------------
+
 if __name__ == "__main__":
 
     opsimdb = os.path.join('/Users', 'danielsf', 'physics')
@@ -29,7 +36,7 @@ if __name__ == "__main__":
             cat = PhoSimCatalogPoint(db, obs_metadata=obs)
             with open(fileName, 'w') as output:
                 cat.write_header(output)
-                output.write("object 0 %.4f %.4f 20 ../sky/sed_flat.txt 0 0 0 0 0 0 output_double.fits 0.2 0.0 none\n" 
+                output.write("object 0 %.4f %.4f 20 ../sky/sed_flat.txt 0 0 0 0 0 0 output_double.fits 0.2 0.0 none\n"
                              % (obs.unrefractedRA, obs.unrefractedDec))
             timeseries.write('%.4f %s\n' % (obs.mjd, obs.bandpass[0]))
 
